b62bfeec8d134e61e8f933e7df9ff38f31216655
MarinaSMarques <166162970+MarinaSMarques@users.noreply.github.com>
2024-11-05 08:35:02 -0300

    Create questão1.c

---
+++ questão1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int indice;
+    int soma =0;
+    int K;
+    indice = 13;
+    printf("Valor de K:");
+    scanf("%d", &K);
+    {
+    while (K < indice)
+    K = (K + 1); 
+    soma = (soma+K);
+    soma = soma;
+    printf("%d", soma);
+    return 0; 
+    }
+}
